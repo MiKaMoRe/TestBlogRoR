c3fed641c53dc696e083840dc28c754cce73b9fe
mikamore <mikamore-inc@gmail.com>
2023-04-30 18:13:59 +0300

    Add tests, models and controllers

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :post do
-    title { Faker::Lorem.sentence(3) }
-    description { Faker::Lorem.sentence(30, false, 10) }
+    title { Faker::Lorem.sentence(word_count: 3) }
+
+    description { Faker::Lorem.sentence(
+      word_count: 30, random_words_to_add: 10
+    ) }
+
     author { create(:user) }
   end
 end
