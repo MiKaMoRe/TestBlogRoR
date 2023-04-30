cda8c5f6eeb9a458e1dc43f01c4458fc6b3d384f
mikamore <mikamore-inc@gmail.com>
2023-04-30 13:24:46 +0300

    Add admin user, admin panel with arctic_admin theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # Active admin routes
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
 
   # Defines the root path route ("/")
