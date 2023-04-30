9c0bc960d8df43b6761b811daddfaf899d8a1a4d
mikamore <mikamore-inc@gmail.com>
2023-04-30 19:29:54 +0300

    Add pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(:author).joins(:author).all
+    @posts = Post.includes(:author).joins(:author).page(params[:page]).per(10)
   end
 
   def show
