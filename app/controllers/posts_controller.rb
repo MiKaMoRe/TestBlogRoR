c99c438774ea64c52ea5b614d11178b6056ceb6d
mikamore <mikamore-inc@gmail.com>
2023-05-04 14:09:00 +0300

    Add active storage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,6 +54,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :image)
   end
 end
